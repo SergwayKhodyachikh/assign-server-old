384808dd71b0ac39947dc91d5e487ba181f731bb
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-05-30 01:03:27 +0300

    integrated error express error handling

---
+++ middleware/ErrorHandle.js
@@ -0,0 +1,30 @@
+const { failure } = require('../util/log');
+const {
+  env: { isProd },
+} = require('../config/env');
+
+/* eslint-disable no-unused-vars */
+
+/**
+ * Handle express errors base on origin, environment and status code
+ * @param {Error} err express error object
+ * @override express default error handler
+ */
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  // log the error if it is unexpected
+  if (!err.isPlaned) failure(err);
+
+  if (isProd)
+    res
+      .status(err.statusCode)
+      .send({ status: err.status, message: err.isPlaned ? err.message : 'internal server error' });
+  else
+    res.status(err.statusCode).send({
+      status: err.status,
+      message: err.message,
+      stack: err.stack.split('\n').map(line => line.trim()),
+    });
+};
