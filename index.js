3195c385e3c991dced8752aebc9790a82068fe56
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2021-07-11 01:24:46 +0300

    dep

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: clientUrl,
-    methods: ['GET', 'POST']
+    origin: '*'
   }
 });
 const express = require('express');
